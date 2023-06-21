77e347ec64c9b668ae6378e406feb7498002dec2
Victor Williams <victor.williams@stu.cu.edu.ng>
2023-06-21 14:18:26 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <div>
+          <h1 className=" p-1 text-black">Backstage Talks</h1>
+          <h3></h3>
+        </div>
+        <div></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
